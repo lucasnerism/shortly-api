9886844772938334e3ea61cec805d775f0e9519f
Lucas Neris <lucasnerism@gmail.com>
2023-05-21 18:35:28 -0300

    urls controller, service and routes implemented

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -5,15 +5,26 @@ const createNewUser = async ({ name, email, hashPassword }) => {
 };
 
 const getUserByEmail = (email) => {
-  return db.query(`SELECT * from users WHERE email=$1;`, [email]);
+  return db.query(`SELECT * FROM users WHERE email=$1;`, [email]);
 };
 
-const getUserById = async (id) => {
+const getUserById = (id) => {
   return db.query(`SELECT * FROM users WHERE id=$1;`, [id]);
+};
+
+const getUser = (id) => {
+  return db.query(`
+  SELECT users.id, users.name,SUM(urls."visitCount") AS "visitCount", 
+  JSON_AGG(urls."id",urls."shortUrl",urls."url",urls."visitCount" GROUP BY urls.id) "shortnedUrls"
+  FROM users
+  INNER JOIN urls USING (id)
+  GROUP BY users.id  
+  ;`, [id]);
 };
 
 export default {
   createNewUser,
   getUserByEmail,
-  getUserById
+  getUserById,
+  getUser
 };
