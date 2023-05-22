e587bf1f36385bd02f837e1caf08f7b1001c8246
Lucas Neris <lucasnerism@gmail.com>
2023-05-22 12:26:52 -0300

    Fix to auto-tests

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -23,7 +23,7 @@ const signIn = async ({ email, password }) => {
 
     const data = { userId: user.id };
     const token = jwt.sign(data, process.env.JWT_KEY, { expiresIn: "1 day" });
-    return { status: 200, response: { token, name: user.name } };
+    return { status: 200, response: { token } };
   } catch (error) {
     return { status: 500, response: { message: error.message } };
   }
